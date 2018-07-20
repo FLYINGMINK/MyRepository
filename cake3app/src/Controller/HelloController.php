5d5bf05422b0162581fde29f366706d965a12565
Minoru Kamishima <kamishimam@sg-ss.jp>
2018-07-20 11:19:30 +0900

    一行追加した

--- cake3app/src/Controller/HelloController.php
+++ cake3app/src/Controller/HelloController.php
@@ -9,6 +9,7 @@ class HelloController extends AppController
         $this->viewBuilder()->autoLayout(true);
         $this->viewBuilder()->Layout('honoka');
         #ブランチ1
+        $this->viewBuilder()->autoLayout(false);
     }
     public function index()
     {
